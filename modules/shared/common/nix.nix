93261c227138409b81b4de3fe0ec995ef256e074
Marco <mrcdsl@proton.me>
2026-04-07 20:19:30 +0000

    fix: reorganize substituters and trusted-substituters in nix configuration

--- modules/shared/common/nix.nix
+++ modules/shared/common/nix.nix
@@ -14,14 +14,25 @@
     settings = let
       mib = 1024 * 1024;
       gib = 1024 * mib;
-    in {
-      min-free = 100 * mib;
-      max-free = 1 * gib; # Keep the explicit 1 for clarity
 
       substituters = [
         "https://cache.nixos.org"
         "https://cachix.cachix.org"
         "https://devenv.cachix.org"
+        "https://marcodsl.cachix.org"
+        "https://nix-community.cachix.org"
+      ];
+    in {
+      inherit substituters;
+
+      min-free = 100 * mib;
+      max-free = 1 * gib; # Keep the explicit 1 for clarity
+
+      trusted-substituters = [
+        "https://cache.nixos.org"
+        "https://cachix.cachix.org"
+        "https://devenv.cachix.org"
+        "https://marcodsl.cachix.org"
         "https://nix-community.cachix.org"
       ];
 
@@ -29,6 +40,7 @@
         "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
         "cachix.cachix.org-1:eWNHQldwUO7G2VkjpnjDbWwy4KQ/HNxht7H4SSoMckM="
         "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+        "marcodsl.cachix.org-1:gH4jaxy05qaIKpJ459Wk4rmDzVhSzVbViwdIsrvlH9k="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       ];
 
