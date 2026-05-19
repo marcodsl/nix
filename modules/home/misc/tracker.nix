691b80f30744640ef0c5c563e5662ac18ca573ac
Marco <mrcdsl@proton.me>
2026-05-19 03:12:30 +0000

    chore(home/misc): drop orphan tracker.nix
    Imports list already excludes ./tracker.nix; the file was left behind. Removing it restores GNOME Tracker3 to its packaged defaults.

--- modules/home/misc/tracker.nix
+++
@@ -1,8 +0,0 @@
-{...}: {
-  dconf.settings."org/freedesktop/Tracker3/Miner/Files" = {
-    crawling-interval = -2;
-    enable-monitors = false;
-    index-recursive-directories = [];
-    index-single-directories = [];
-  };
-}
