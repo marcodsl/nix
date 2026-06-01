ac83de23418cb363effa6659952cabec4a86f091
Marco <mrcdsl@proton.me>
2026-06-01 02:53:12 +0000

    feat(fonts): add nerd-fonts and sans-serif families

--- modules/shared/system/fonts.nix
+++ modules/shared/system/fonts.nix
@@ -1,14 +1,26 @@
 {pkgs, ...}: {
   fonts.packages = with pkgs;
     (with nerd-fonts; [
+      blex-mono
       geist-mono
       iosevka
+      iosevka-term
+      iosevka-term-slab
       jetbrains-mono
       monaspace
       symbols-only
+      tinos
       zed-mono
     ])
+    ++ (
+      with ioskeley-mono; [
+        normal-NF
+      ]
+    )
     ++ [
       inter
+      work-sans
+      googlesans-code
+      dm-sans
     ];
 }
